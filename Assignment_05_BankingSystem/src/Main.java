11ae8fbc745d74bfc7249af5084dc9c119d5cac2
Udit Padhan <105899769+uditpadhan98@users.noreply.github.com>
2025-01-28 08:35:36 +0530

    Update Main.java

--- Assignment_05_BankingSystem/src/Main.java
+++ Assignment_05_BankingSystem/src/Main.java
@@ -4,19 +4,19 @@ public class Main {
         TransactionProcessor processor = new TransactionProcessor(bank);
 
         // Create accounts
-        BankAccount account1 = new BankAccount("A101", "John Doe", 1000, "Saving");
-        BankAccount account2 = new BankAccount("A102", "Jane Smith", 2000, "Current");
+        BankAccount account1 = new BankAccount("123", "Udit", 3000, "Saving");
+        BankAccount account2 = new BankAccount("234", "Sachin", 2000, "Current");
         bank.addAccount(account1);
         bank.addAccount(account2);
 
         // Simulate transactions
-        processor.processTransaction("Deposit", 500, "A101", null); // Deposit into A101
-        processor.processTransaction("Withdraw", 300, "A102", null); // Withdraw from A102
-        processor.processTransaction("Transfer", 200, "A101", "A102"); // Transfer from A101 to A102
+        processor.processTransaction("Deposit", 500, "123", null);
+        processor.processTransaction("Withdraw", 200, "123", null);
+        processor.processTransaction("Transfer", 500, "123", "234");
 
         // Allow threads to complete
         try {
-            Thread.sleep(2000); // Give threads time to finish for demo purposes
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
